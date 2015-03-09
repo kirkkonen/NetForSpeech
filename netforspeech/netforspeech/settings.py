4a4e65a1fab75869cdf9ac4b6b77791f82c681d8
Krom <krommail@gmail.com>
2015-03-09 23:28:56 +0300

    heroku deploy fix

--- netforspeech/netforspeech/settings.py
+++ netforspeech/netforspeech/settings.py
@@ -111,4 +111,14 @@ STATIC_URL = '/static/'
 
 
 # EXPERIMENTAL PRODUCTION SETTINGS
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
