c75c5f721c1a9292610427486aadecd840176d90
Krom <krommail@gmail.com>
2015-03-10 01:13:29 +0300

    heroku deploy fix

--- netforspeech/netforspeech/settings.py
+++ netforspeech/netforspeech/settings.py
@@ -40,7 +40,7 @@ DJANGO_APPS = (
 )
 
 HOMEBREW_APPS = (
-    'nfsmain',
+    'netforspeech.nfsmain',
 )
 
 INSTALLED_APPS = DJANGO_APPS + HOMEBREW_APPS
@@ -113,6 +113,9 @@ STATIC_URL = '/static/'
 # EXPERIMENTAL PRODUCTION SETTINGS
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
