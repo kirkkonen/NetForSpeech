a7f20ae02507dc6ccb0f3f24a376eeb3997eb060
Krom <krommail@gmail.com>
2015-03-10 22:24:58 +0300

    heroku deploy fix

--- manage.py
+++ manage.py
@@ -3,7 +3,11 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "netforspeech.settings.dev_settings")
+    # May be it is better to set a special env on Heroku
+    if 'DYNO' in os.environ:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "netforspeech.settings.prod_settings")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "netforspeech.settings.dev_settings")
 
     from django.core.management import execute_from_command_line
 
--- netforspeech/wsgi.py
+++ netforspeech/wsgi.py
@@ -11,9 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-if "DJANGO_SETTINGS_MODULE" in os.environ.keys():
-    application = get_wsgi_application()
-else:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "netforspeech.settings.prod_settings")
+# May be it is better to set a special env on Heroku
+if 'DYNO' in os.environ:
     from dj_static import Cling
     application = Cling(get_wsgi_application())
+else:
+    application = get_wsgi_application()
