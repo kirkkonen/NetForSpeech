15fe37d3379d88da02e9012a4157ca220f3f8972
Krom <krommail@gmail.com>
2015-03-10 22:08:27 +0300

    heroku deploy fix and project restructure

--- netforspeech/wsgi.py
+++ netforspeech/wsgi.py
@@ -14,6 +14,6 @@ from django.core.wsgi import get_wsgi_application
 if "DJANGO_SETTINGS_MODULE" in os.environ.keys():
     application = get_wsgi_application()
 else:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "netforspeech.prod_settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "netforspeech.settings.prod_settings")
     from dj_static import Cling
     application = Cling(get_wsgi_application())
